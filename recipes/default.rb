0b7f05667e3b701131697077ce366e8ca1f3a2b5
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-10-05 14:45:29 -1000

    test install on env

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,65 +18,69 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# s3t package install:
-package 's3ql' do
- action :install
-end
+if node['chef-s3ql']['authfilePath']
 
-directory '/root/.s3ql' do
- owner 'root'
- group 'root'
- mode  '0700'
- action :create
-end
+ # s3t package install:
+ package 's3ql' do
+  action :install
+ end
 
-# fstab update:
-content = ''
-Array[ node['chef-s3ql']['sections'] ].each do |section|
- content += '[' + section['name'] + ']' + "\n"
- content += 'backend-login: ' + section['backend-login'] + "\n"        if section['backend-login']
- content += 'backend-password: ' + section['backend-password'] + "\n"  if section['backend-password']
- content += 'storage-url: ' + section['storage-url'] + "\n"            if section['storage-url']
- content += 'fs-passphrase: ' + section['fs-passphrase'] + "\n"        if section['fs-passphrase']
- content += "\n"
+ directory '/root/.s3ql' do
+  owner 'root'
+  group 'root'
+  mode  '0700'
+  action :create
+ end
 
- if section['device'] && section['mountPoint']
-  bash 'fstab update' do
-   code "grep -qsw \"#{section['device']}\" /etc/fstab || echo \"##{section['device']} #{section['mountPoint']} #{section['options']}\" >>/etc/fstab"
-  end
+ # fstab update:
+ content = ''
+ Array[ node['chef-s3ql']['sections'] ].each do |section|
+  content += '[' + section['name'] + ']' + "\n"
+  content += 'backend-login: ' + section['backend-login'] + "\n"        if section['backend-login']
+  content += 'backend-password: ' + section['backend-password'] + "\n"  if section['backend-password']
+  content += 'storage-url: ' + section['storage-url'] + "\n"            if section['storage-url']
+  content += 'fs-passphrase: ' + section['fs-passphrase'] + "\n"        if section['fs-passphrase']
+  content += "\n"
 
-  directory section['mountPoint'] do
-   owner 'root'
-   group 'root'
-   mode  '0700'
-   action :create
+  if section['device'] && section['mountPoint']
+   bash 'fstab update' do
+    code "grep -qsw \"#{section['device']}\" /etc/fstab || echo \"##{section['device']} #{section['mountPoint']} #{section['options']}\" >>/etc/fstab"
+   end
+
+   directory section['mountPoint'] do
+    owner 'root'
+    group 'root'
+    mode  '0700'
+    action :create
+   end
   end
  end
-end
 
-# authinfo file:
-file node['chef-s3ql']['authfilePath'] do
- content content
- mode  '0600'
- owner 'root'
- group 'root'
-end
+ # authinfo file:
+ file node['chef-s3ql']['authfilePath'] do
+  content content
+  mode  '0600'
+  owner 'root'
+  group 'root'
+ end
 
-# service install:
-cookbook_file "/etc/init.d/mount-s3ql" do
- source 'mount-s3ql.sh'
- owner 'root'
- group 'root'
- mode '0755'
- action :create
- notifies :reload, 'service[mount-s3ql]', :immediately
-end
+ # service install:
+ cookbook_file "/etc/init.d/mount-s3ql" do
+  source 'mount-s3ql.sh'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+  notifies :reload, 'service[mount-s3ql]', :immediately
+ end
+ 
+ # service declaration:
+ service "mount-s3ql" do
+  start_command "/etc/init.d/mount-s3ql start"
+  status_command "/etc/init.d/mount-s3ql status"
+  stop_command "/etc/init.d/mount-s3ql stop"
+  supports :start => true, :stop => true, :status => true
+  action [ :start, :enable ]
+ end
 
-# service declaration:
-service "mount-s3ql" do
- start_command "/etc/init.d/mount-s3ql start"
- status_command "/etc/init.d/mount-s3ql status"
- stop_command "/etc/init.d/mount-s3ql stop"
- supports :start => true, :stop => true, :status => true
- action [ :start, :enable ]
 end
